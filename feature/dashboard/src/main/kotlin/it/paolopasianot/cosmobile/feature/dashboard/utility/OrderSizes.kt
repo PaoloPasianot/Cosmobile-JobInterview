dda8b0d34bca6fc06b321485c0ee50c6b4dde571
PaoloPasianot <115974374+PaoloPasianot@users.noreply.github.com>
2024-11-27 15:38:21 +0100

    fix

--- feature/dashboard/src/main/kotlin/it/paolopasianot/cosmobile/feature/dashboard/utility/OrderSizes.kt
+++ feature/dashboard/src/main/kotlin/it/paolopasianot/cosmobile/feature/dashboard/utility/OrderSizes.kt
@@ -3,11 +3,11 @@ package it.paolopasianot.cosmobile.feature.dashboard.utility
 object OrderSizes {
 
   private val sizeTypes = listOf(
-    SizeType(matchPattern = "\\d+".toRegex(), order = 1),
-    SizeType(matchPattern = "[XSML]+".toRegex(), order = 2),
-    SizeType(matchPattern = "IT \\d+".toRegex(), order = 3),
-    SizeType(matchPattern = "FR \\d+".toRegex(), order = 4),
-    SizeType(matchPattern = "UK \\d+".toRegex(), order = 5),
+    SizeType(matchPattern = "\\d+".toRegex(), order = GroupOrder.NUMBER),
+    SizeType(matchPattern = "[XSML]+".toRegex(), order = GroupOrder.SIZE),
+    SizeType(matchPattern = "IT \\d+".toRegex(), order = GroupOrder.IT),
+    SizeType(matchPattern = "FR \\d+".toRegex(), order = GroupOrder.FR),
+    SizeType(matchPattern = "UK \\d+".toRegex(), order = GroupOrder.UK),
   )
 
   fun order(unorderedSizes: List<String>): List<String> {
@@ -19,16 +19,40 @@ object OrderSizes {
       }
       .groupBy(SizeType::order)
       .toSortedMap()
-      .mapValues { (_, values) -> values.sortedBy(SizeType::value) }
+      .mapValues { (order, values) ->
+        values.sortedWith(
+          order.comparator ?: compareBy { it.value },
+        )
+      }
       .flatMap { it.value }
       .map(SizeType::value)
 
   }
 
+  internal enum class GroupOrder(
+    val comparator: Comparator<SizeType>? = null,
+  ) {
+    NUMBER,
+    SIZE(
+      comparator = compareBy{
+        when {
+          it.value.matches(Regex("^S.*")) -> 0
+          it.value.matches(Regex("^M.*")) -> 1
+          it.value.matches(Regex("^L.*")) -> 2
+          else -> 3
+        }
+      },
+    ),
+    IT,
+    FR,
+    UK,
+  }
+
   internal data class SizeType(
     val matchPattern: Regex,
     val value: String = "",
-    val order: Int = 0,
+    val order: GroupOrder,
+    val comparator: Comparator<String>? = null,
   )
 
 }
--- feature/navigation/src/main/kotlin/it/paolopasianot/cosmobile/feature/navigation/destination/dashboard/DashboardDestination.kt
+++ feature/navigation/src/main/kotlin/it/paolopasianot/cosmobile/feature/navigation/destination/dashboard/DashboardDestination.kt
@@ -1,5 +1,7 @@
 package it.paolopasianot.cosmobile.feature.navigation.destination.dashboard
 
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
@@ -13,18 +15,20 @@ import org.koin.androidx.compose.koinViewModel
  * Show che dashboard
  */
 internal fun NavGraphBuilder.dashboardScreen(
-  modifier: Modifier = Modifier
-){
-  composable<Destination.Dashboard>{
-    val  viewModel = koinViewModel<DashboardViewModel>()
+  modifier: Modifier = Modifier,
+) {
+  composable<Destination.Dashboard> {
+    val viewModel = koinViewModel<DashboardViewModel>()
 
     val (state, event, effect) = use(viewModel = viewModel)
 
     DashboardUI(
-      modifier = modifier,
+      modifier = modifier
+        .systemBarsPadding()
+        .navigationBarsPadding(),
       state = state,
       event = event,
-      effects = effect
+      effects = effect,
     )
   }
 }
